589bfdef83b837e3ccce79e9bd7ce0d4bcc65c9f
CloudChen <im_sure@163.com>
2016-10-24 22:56:27 +0800

    Revise 371

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class LeetCode {
 	public static void main(String[] args){
 		_371SumofTwoIntegers test = new _371SumofTwoIntegers();
-		int result = test.getSum(-1,-5);
+		int result = test.getSum(5234,102);
 		System.out.println(result);
 	}
 }
--- src/_371SumofTwoIntegers.java
+++ src/_371SumofTwoIntegers.java
@@ -1,15 +1,13 @@
 
 public class _371SumofTwoIntegers {
 	public int getSum(int a, int b) {
-		int sum = 0;
 		int c = (a & b) << 1;
 		int d = a ^ b;
         while (c != 0) {
-        	sum = d;
+        	int tmp = d;
         	d = d ^ c;
-        	c = (c & sum) << 1;
+        	c = (c & tmp) << 1;
         }
-        sum = d;
-        return sum;
+        return d;
     }
 }
