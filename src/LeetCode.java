bc5654d28d4085d9179595b86940df6f4493f86d
CloudChen <im_sure@163.com>
2016-10-24 21:28:28 +0800

    Add 136 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_292NimGame test = new _292NimGame();
-		boolean result = test.canWinNim(11);
+		_136SingleNumber test = new _136SingleNumber();
+		int result = test.singleNumber(new int[] {0,2,1,4,1,0,5,6,6,5,2});
 		System.out.println(result);
 	}
 }
---
+++ src/_136SingleNumber.java
@@ -0,0 +1,8 @@
+
+public class _136SingleNumber {
+	public int singleNumber(int[] nums) {
+        int singleNum = 0;
+        for (int i = 0; i < nums.length; i++) singleNum ^= nums[i];
+        return singleNum;
+    }
+}
