7b799cf8c3032f89152b87d73561b8d29c91bfbc
CloudChen <im_sure@163.com>
2016-10-28 21:16:37 +0800

    Add 97 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,11 +2,12 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_349IntersectionOfTwoArrays test = new _349IntersectionOfTwoArrays();
-		int[] a = {5,1,7,3};
-		int[] b = {3,5};
-		int[] result = test.intersection(a, b);
-		for (int i : result)
-			System.out.println(i);
+		_97InterleavingString test = new _97InterleavingString();
+		String s1 = "aabcc";
+		String s2 = "dbbca";
+		String s3 = "aadbbbaccc";
+		boolean result = test.isInterleave(s1, s2, s3);
+		System.out.println(result);
+		
 	}
 }
