af63d6b90cd4e45c31f3ff302cb49d565c6c239b
CloudChen <im_sure@163.com>
2016-11-01 20:43:51 +0800

    Add 217 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_409LongestPalindrome test = new _409LongestPalindrome();
-		String a = "abAACbDRR";
-		int result = test.longestPalindrome(a);
+		_217ContainsDuplicate test = new _217ContainsDuplicate();
+		int[] a = {};
+		boolean result = test.containsDuplicate(a);
 		System.out.println(result);
 	}
 }
