d1e38bb2c03d343f4d94fefd5abb5e599fe2538d
CloudChen <im_sure@163.com>
2016-11-01 22:16:12 +0800

    Add 350 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,11 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_217ContainsDuplicate test = new _217ContainsDuplicate();
-		int[] a = {};
-		boolean result = test.containsDuplicate(a);
-		System.out.println(result);
+		_350IntersectionOfTwoArraysII test = new _350IntersectionOfTwoArraysII();
+		int[] a = {1,2,2,1};
+		int[] b = {1,1};
+		int[] result = test.intersect(a, b);
+		for (int i = 0; i < result.length; i++)
+			System.out.println(result[i]);
 	}
 }
---
+++ src/_350IntersectionOfTwoArraysII.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _350IntersectionOfTwoArraysII {
+	public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length == 0 || nums2.length == 0) return new int[0];
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> r = new ArrayList<Integer>();
+        int i = 0, j = 0;
+        while(i != nums1.length && j != nums2.length) {
+        	if (nums1[i] == nums2[j]) {
+        		r.add(nums1[i]);
+        		i++;
+        		j++;
+        	} else if (nums1[i] < nums2[j]) i++;
+        	else j++;
+        }
+        if (r.isEmpty()) return new int[0];
+        else {
+        	int[] result = new int[r.size()];
+        	for (int h = 0; h < r.size(); h++) {
+        		result[h] = r.get(h);
+        	}
+            return result;
+        }
+    }
+}
