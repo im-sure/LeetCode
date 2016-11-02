9883d208be68d77ebc0481ae49103d4abff095a2
CloudChen <im_sure@163.com>
2016-11-02 21:29:58 +0800

    Add 13 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,8 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_401BinaryWatch test = new _401BinaryWatch();
-		List<String> result = test.readBinaryWatch(1);
-		for (int i = 0; i < result.size(); i++)
-			System.out.println(result.get(i));
+		_013RomanToInteger test = new _013RomanToInteger();
+		int result = test.romanToInt("DCXXI");
+		System.out.println(result);
 	}
 }
