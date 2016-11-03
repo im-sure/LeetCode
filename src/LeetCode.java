ec92ef021126900c94e718448867cf8ab9f88df2
CloudChen <im_sure@163.com>
2016-11-03 15:56:31 +0800

    Add 206 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,8 +2,23 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_013RomanToInteger test = new _013RomanToInteger();
-		int result = test.romanToInt("DCXXI");
-		System.out.println(result);
+		_206ReverseLinkedList test = new _206ReverseLinkedList();
+		_206ReverseLinkedList.ListNode l1 = test.new ListNode(1);
+		_206ReverseLinkedList.ListNode l2 = test.new ListNode(2);
+		_206ReverseLinkedList.ListNode l3 = test.new ListNode(3);
+		_206ReverseLinkedList.ListNode l4 = test.new ListNode(4);
+		_206ReverseLinkedList.ListNode l5 = test.new ListNode(5);
+		_206ReverseLinkedList.ListNode l6 = test.new ListNode(6);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l5;
+		l5.next = l6;
+		_206ReverseLinkedList.ListNode result = test.reverseList(l1);
+		while (result.next != null) {
+			System.out.println(result.val);
+			result = result.next;
+		}
+		System.out.println(result.val);
 	}
 }
---
+++ src/_206ReverseLinkedList.java
@@ -0,0 +1,30 @@
+
+public class _206ReverseLinkedList {
+	public ListNode reverseList(ListNode head) {
+		if (head == null) return null;
+		ListNode tmp = null;
+		ListNode tmpNext = null;
+		ListNode tmpNextNext = null;
+        while (head.next != null && head.next.next != null) {
+        	tmpNextNext = head.next.next;
+        	tmpNext = head.next;
+        	head.next.next = head;
+        	head.next = tmp;
+        	tmp = tmpNext;
+        	head = tmpNextNext;
+        }
+        if (head.next != null) {
+        	tmpNext = head.next;
+        	head.next.next = head;
+        	head.next = tmp;
+        	head = tmpNext;
+        } else head.next = tmp;
+        return head;
+    }
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+}
