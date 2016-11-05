495820a0a168bd474d8c0fac4e5eff831aee437f
CloudChen <im_sure@163.com>
2016-11-05 20:26:19 +0800

    Revise 202

--- src/_202HappyNumber.java
+++ src/_202HappyNumber.java
@@ -5,10 +5,13 @@ public class _202HappyNumber {
 	public boolean isHappy(int n) {
         List<Integer> list = new ArrayList<Integer>();
         while (!list.contains(n)) {
-        	 list.add(n);
+        	list.add(n);
         	int tmp = 0;
-        	for (int i = 0; i < String.valueOf(n).length(); i++)
-        		tmp += Math.pow((n / (int)Math.pow(10, i) % 10), 2);
+        	int l = String.valueOf(n).length();
+        	for (int i = 0; i < l; i++){
+        		tmp += Math.pow(n % 10, 2);
+        		n = n / 10;
+        	}
     		n = tmp;
         	if (n == 1) return true;
         }
