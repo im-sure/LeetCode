39214152a532f43d609f6ff769f93353ff7883ab
CloudChen <im_sure@163.com>
2016-11-15 20:48:45 +0800

    Add 101 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,29 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args){
-		_345ReverseVowelsOfAString test = new _345ReverseVowelsOfAString();
-		String a = "aA";
-		String result = test.reverseVowels(a);
-		System.out.println(result.toString());
+		_101SymmetricTree test = new _101SymmetricTree();
+		_101SymmetricTree.TreeNode one = test.new TreeNode(1);
+		_101SymmetricTree.TreeNode two1 = test.new TreeNode(2);
+		_101SymmetricTree.TreeNode two2 = test.new TreeNode(2);
+		_101SymmetricTree.TreeNode three1 = test.new TreeNode(3);
+		_101SymmetricTree.TreeNode four1 = test.new TreeNode(4);
+		_101SymmetricTree.TreeNode three2 = test.new TreeNode(3);
+		_101SymmetricTree.TreeNode four2 = test.new TreeNode(4);
+		_101SymmetricTree.TreeNode five1 = test.new TreeNode(5);
+		_101SymmetricTree.TreeNode five2 = test.new TreeNode(5);
+		_101SymmetricTree.TreeNode six1 = test.new TreeNode(6);
+		_101SymmetricTree.TreeNode six2 = test.new TreeNode(6);
+		one.left = two1;
+		one.right = two2;
+		two1.left = three1;
+		two1.right = four1;
+		two2.left = four2;
+		two2.right = three2;
+		four1.left = five1;
+		four1.right = six1;
+		four2.left = six2;
+		four2.right = five2;
+		boolean result = test.isSymmetric(one);
+		System.out.println(result);
 	}
 }
---
+++ src/_101SymmetricTree.java
@@ -0,0 +1,31 @@
+
+public class _101SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+    	if (root == null) return true;
+    	else return values(root.left, true).equals(values(root.right, false));
+    }
+    
+    public String values(TreeNode root, boolean fromLeft) {
+    	if (root == null) return "";
+    	String v = "" + root.val;
+    	if (fromLeft) {
+    		if (root.left != null) v += values(root.left, true);
+    		else v += "a";
+    		if (root.right != null) v += values(root.right, true);
+    		else v += "a";
+    	} else {
+    		if (root.right != null) v += values(root.right, false);
+    		else v += "a";
+    		if (root.left != null) v += values(root.left, false);
+    		else v += "a";
+    	}
+    	return v;
+    }
+    
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+}
