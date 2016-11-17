3d7e97277de269abadc9670c582bc2f1824775f4
CloudChen <im_sure@163.com>
2016-11-17 20:28:53 +0800

    Add 118 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,11 +1,10 @@
 import java.util.List;
 
 public class LeetCode {
-	public static void main(String[] args){
-		_066PlusOne test = new _066PlusOne();
-		int[] a = {9,5,1};
-		int[] result = test.plusOne(a);
-		for (int i = 0; i < result.length; i++)
-			System.out.print(result[i]);
+	public static void main(String[] args) {
+		_118PascalsTriangle test = new _118PascalsTriangle();
+		int a = 5;
+		List<List<Integer>> result = test.generate(a);
+		System.out.print(result.toString());
 	}
 }
