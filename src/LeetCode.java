b9f5e68bb315e272508385c58c933ce7abbb3670
CloudChen <im_sure@163.com>
2016-11-22 20:10:14 +0800

    Add 9 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_172FactorialTrailingZeroes test = new _172FactorialTrailingZeroes();
-		int a = 12;
-		int result = test.trailingZeroes(a);
+		_009PalindromeNumber test = new _009PalindromeNumber();
+		int a = 1235321;
+		boolean result = test.isPalindrome(a);
 		System.out.println(result);
 	}
 }
