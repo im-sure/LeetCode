24f6dbf84a95f2658b2a84b886abe15643f930d4
CloudChen <im_sure@163.com>
2016-11-24 20:41:12 +0800

    Add 205 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,10 +2,10 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_299BullsAndCows test = new _299BullsAndCows();
-		String a = "00112233445566778899";
-		String b = "16872590340158679432";
-		String result = test.getHint(a, b);
+		_205IsomorphicStrings test = new _205IsomorphicStrings();
+		String a = "ab";
+		String b = "aa";
+		boolean result = test.isIsomorphic(a, b);
 		System.out.println(result);
 	}
 }
---
+++ src/_205IsomorphicStrings.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class _205IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() == 0) return true;
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        for (int i = 0; i < s.length(); i++) {
+        	char cs = s.charAt(i);
+        	char ct = t.charAt(i);
+        	if (map.containsKey(cs)) {
+        		if (map.get(cs) != ct) return false;
+        	} else if (map.containsValue(ct)) return false;
+        	else map.put(cs, ct);
+        }
+        return true;
+    }
+}
