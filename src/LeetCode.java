e66189939c779dfa1e264dd9c21a2411798614e6
CloudChen <im_sure@163.com>
2016-11-25 17:03:22 +0800

    Add 19 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,11 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_038CountAndSay test = new _038CountAndSay();
-		int a = 7;
-		String result = test.countAndSay(a);
+		_019RemoveNthNodeFromEndOfList test = new _019RemoveNthNodeFromEndOfList();
+		_019RemoveNthNodeFromEndOfList.ListNode a = test.new ListNode(1);
+		_019RemoveNthNodeFromEndOfList.ListNode b = test.new ListNode(2);
+		a.next = b;
+		_019RemoveNthNodeFromEndOfList.ListNode result = test.removeNthFromEnd(a, 1);
 		System.out.println(result);
 	}
 }
---
+++ src/_019RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class _019RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        List<ListNode> list = new LinkedList<ListNode>();
+        list.add(head);
+        ListNode node = head.next;
+        int count = 1;
+        while (node != null) {
+        	list.add(node);
+        	if (count == n + 1) list.remove(0);
+        	else count++;
+        	node = node.next;
+        }
+        if (count == 1) return null;
+        else if (count == n) return head.next;
+        else if (list.size() >= 3) list.remove(0).next = list.remove(1);
+        else list.remove(0).next = null;
+        return head;
+    }
+    
+    public class ListNode {
+    	int val;
+    	ListNode next;
+    	ListNode(int x) { val = x; }
+    }
+}
