79c0c8b483b93270c3a5ceec972a5a5314b6ffff
CloudChen <im_sure@163.com>
2016-11-28 21:11:47 +0800

    Add 20 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,10 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_290WordPattern test = new _290WordPattern();
-		String a = "abba";
-		String b = "dog cat cat dog";
-		boolean result = test.wordPattern(a, b);
+		_020ValidParentheses test = new _020ValidParentheses();
+		String a = "[(])";
+		boolean result = test.isValid(a);
 		System.out.println(result);
 	}
 }
---
+++ src/_020ValidParentheses.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class _020ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+        	char c = s.charAt(i);
+        	if (c == '(') stack.push(')');
+        	else if (c == '[') stack.push(']');
+        	else if (c == '{') stack.push('}');
+        	else if (stack.isEmpty() || c != stack.pop()) return false;
+        }
+        if (stack.isEmpty()) return true;
+        else return false;
+    }
+}
