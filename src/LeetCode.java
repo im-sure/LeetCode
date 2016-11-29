70a836332852f8d4213fb9f1a20fc686f0945584
CloudChen <im_sure@163.com>
2016-11-29 19:56:30 +0800

    Add 219 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,10 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_020ValidParentheses test = new _020ValidParentheses();
-		String a = "[(])";
-		boolean result = test.isValid(a);
+		_219ContainsDuplicateII test = new _219ContainsDuplicateII();
+		int[] a = {1,2,3,1,4,1};
+		int b = 2;
+		boolean result = test.containsNearbyDuplicate(a, b);
 		System.out.println(result);
 	}
 }
