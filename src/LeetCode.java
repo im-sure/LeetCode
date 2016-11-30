6af68f6249bc0dd14108fb69179528733b08ff8a
CloudChen <im_sure@163.com>
2016-11-30 20:59:32 +0800

    Add 225 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,12 +2,18 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_088MergeSortedArray test = new _088MergeSortedArray();
-		int[] a = {0,1,2,2,3,4,5,6,6,8,0,0,0,0,0,0,0};
-		int b = 10;
-		int[] c = {2,3,3,4,7,8,9};
-		int d = 7;
-		test.merge(a, b, c, d);
+		_225ImplementStackUsingQueues test = new _225ImplementStackUsingQueues();
+		test.push(1);
+		test.push(2);
+		test.push(3);
+		test.top();
+		test.pop();
+		test.top();
+		test.pop();
+		test.top();
+		test.empty();
+		test.pop();
+		test.empty();
 		//System.out.println(a);
 	}
 }
