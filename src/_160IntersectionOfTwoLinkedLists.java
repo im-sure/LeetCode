b257e05e369f8bbd2d67e6a494f40d9e90b2745a
CloudChen <im_sure@163.com>
2016-12-04 14:59:50 +0800

    Add 160 solution

---
+++ src/_160IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,22 @@
+
+public class _160IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null) return null;
+		ListNode a = headA;
+		ListNode b = headB;
+		while (a != b) {
+			a = a == null ? headB : a.next;
+			b = b == null ? headA : b.next;
+		}
+		return a;
+    }
+    
+    class ListNode {
+    	int val;
+    	ListNode next;
+    	ListNode(int x) {
+    		val = x;
+    		next = null;
+    	}
+    }
+}
