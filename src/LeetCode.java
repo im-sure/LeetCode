827f4be0315ddc8c81d553eb1087e9eb19167a5a
CloudChen <im_sure@163.com>
2016-12-06 19:21:50 +0800

    Add 28 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,10 +2,10 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_001TwoSum test = new _001TwoSum();
-		int[] a = {0,3,4,0};
-		int b = 0;
-		int[] result = test.twoSum(a, b);
-		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
+		_028ImplementStrStr test = new _028ImplementStrStr();
+		String a = "412";
+		String b = "12";
+		int result = test.strStr(a, b);
+		System.out.println(result);
 	}
 }
---
+++ src/_028ImplementStrStr.java
@@ -0,0 +1,12 @@
+
+public class _028ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        int lh = haystack.length();
+		int ln = needle.length();
+        if (lh < ln) return -1;
+        for (int i = 0; i <= lh - ln; i++) {
+        	if (haystack.substring(i, i + ln).equals(needle)) return i;
+        }
+        return -1;
+    }
+}
