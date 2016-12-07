4e0ab5461261209b98979360d2d9fb01be4a460d
CloudChen <im_sure@163.com>
2016-12-07 21:13:01 +0800

    Add 155 solution

---
+++ src/_155MinStack.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _155MinStack {
+	int size;
+	List<Integer> list;
+	int min;
+	
+    public _155MinStack() {
+        size = 0;
+        list = new ArrayList<Integer>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+    	if (x < min) min = x;
+        list.add(x);
+        size++;
+    }
+    
+    public void pop() {
+    	if (min == list.remove(size-- - 1)) {
+    		min = Integer.MAX_VALUE;
+			for (int i = 0; i < size; i++) {
+				if (min > list.get(i)) min = list.get(i);
+			}
+    	}
+    }
+    
+    public int top() {
+        return list.get(size - 1);
+    }
+    
+    public int getMin() {
+    	return min;
+    }
+}
