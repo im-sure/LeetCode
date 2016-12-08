89c1641a9fea17eeab230a0472129310c7a38528
CloudChen <im_sure@163.com>
2016-12-08 21:27:43 +0800

    Add 278 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_168ExcelSheetColumnTitle test = new _168ExcelSheetColumnTitle();
-		int a = 27;
-		String result = test.convertToTitle(a);
+		_278FirstBadVersion test = new _278FirstBadVersion(3);
+		int a = 6;
+		int result = test.firstBadVersion(a);
 		System.out.println(result);
 	}
 }
---
+++ src/_278FirstBadVersion.java
@@ -0,0 +1,14 @@
+
+public class _278FirstBadVersion {
+    public int firstBadVersion(int n) {
+    	int h = n;
+    	int l = 0;
+    	int r = h + (l - h) / 2;
+        while (h - l > 1) {
+        	if (isBadVersion(r)) h = r;
+        	else l = r;
+        	r = h + (l - h) / 2;
+        }
+        return r;
+    }
+}
