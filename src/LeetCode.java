8aa34fb638ef0891706d5e9e4022801379c21271
CloudChen <im_sure@163.com>
2016-12-14 21:55:13 +0800

    Add 338 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_008StringtoIntegerATOI test = new _008StringtoIntegerATOI();
-		String a = "  - 321";
-		int result = test.myAtoi(a);
-		System.out.println(result);
+		_338CountingBits test = new _338CountingBits();
+		int a = 5;
+		int[] result = test.countBits(a);
+		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
 	}
 }
