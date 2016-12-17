a5f852c8febf3722dbdcd8f4653e9d30b61dcafb
CloudChen <im_sure@163.com>
2016-12-17 20:18:23 +0800

    Add 413 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_338CountingBits test = new _338CountingBits();
-		int a = 5;
-		int[] result = test.countBits(a);
-		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
+		_413ArithmeticSlices test = new _413ArithmeticSlices();
+		int[] a = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+		int result = test.numberOfArithmeticSlices(a);
+		System.out.println(result);
 	}
 }
