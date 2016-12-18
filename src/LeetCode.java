dafef7e51879892d0f65d9e864e8f8e499898c3e
CloudChen <im_sure@163.com>
2016-12-18 20:21:47 +0800

    Add 260 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_462MinimumMovesToEqualArrayElementsII test = new _462MinimumMovesToEqualArrayElementsII();
-		int[] a = {1,0,0,8,6};
-		int result = test.minMoves2(a);
-		System.out.println(result);
+		_260SingleNumberIII test = new _260SingleNumberIII();
+		int[] a = {1,2,1,3,2,5};
+		int[] result = test.singleNumber(a);
+		System.out.println(result[0] + " " + result[1]);
 	}
 }
---
+++ src/_260SingleNumberIII.java
@@ -0,0 +1,19 @@
+
+public class _260SingleNumberIII {
+    public int[] singleNumber(int[] nums) {
+        int l = nums.length;
+    	int xor = 0;
+    	for (int i = 0; i < l; i++) xor ^= nums[i];
+    	for (int i = 0; i < 32; i++) {
+    		int bit = 1 << i;
+    		if ((xor & bit) != 0) xor = bit;
+    	}
+    	int xor0 = 0;
+    	int xor1 = 0;
+    	for (int i = 0; i < l; i++) {
+    		if ((nums[i] & xor) == 0) xor0 ^= nums[i];
+    		else xor1 ^= nums[i];
+    	}
+    	return new int[]{xor0, xor1};
+    }
+}
