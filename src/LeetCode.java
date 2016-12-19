ed8a0a809311a5f9e520989a24d89a3f9af28879
CloudChen <im_sure@163.com>
2016-12-19 21:09:53 +0800

    Add 238 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_167TwoSumIIInputArrayIsSorted test = new _167TwoSumIIInputArrayIsSorted();
-		int[] a = {2, 3, 4};
-		int[] result = test.twoSum(a, 6);
-		System.out.println(result[0] + " " + result[1]);
+		_238ProductOfArrayExceptSelf test = new _238ProductOfArrayExceptSelf();
+		int[] a = {1,2, 3, 4};
+		int[] result = test.productExceptSelf(a);
+		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
 	}
 }
