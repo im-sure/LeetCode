4d9bfb8f6bd43a824a00200141c9e813d4529943
CloudChen <im_sure@163.com>
2016-12-20 20:46:41 +0800

    Add 347 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_238ProductOfArrayExceptSelf test = new _238ProductOfArrayExceptSelf();
-		int[] a = {1,2, 3, 4};
-		int[] result = test.productExceptSelf(a);
-		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
+		_347TopKFrequentElements test = new _347TopKFrequentElements();
+		int[] a = {1};
+		List<Integer>  result = test.topKFrequent(a, 1);
+		for (int i : result) System.out.println(i);
 	}
 }
