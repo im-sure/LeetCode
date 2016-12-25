db25b05a64665ca1889b09ac247e83c3b865aa33
CloudChen <im_sure@163.com>
2016-12-25 20:34:20 +0800

    Add 392 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,10 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_343IntegerBreak test = new _343IntegerBreak();
-		int a = 54;
-		int result = test.integerBreak(a);
+		_392IsSubsequence test = new _392IsSubsequence();
+		String a = "abc";
+		String b = "ahbgdc";
+		boolean result = test.isSubsequence(a, b);
 		System.out.println(result);
 	}
 }
---
+++ src/_392IsSubsequence.java
@@ -0,0 +1,18 @@
+
+public class _392IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int ls = s.length();
+        int lt = t.length();
+        if (ls > lt) return false;
+        int iS = 0;
+        int iT = 0;
+        while (iS < ls) {
+        	if (iT == lt || ls - iS > lt - iT) return false;
+        	if (s.charAt(iS) == t.charAt(iT)) {
+        		iS++;
+        		iT++;
+        	} else iT++;
+        }
+        return true;
+    }
+}
