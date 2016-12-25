cdf1e2f1bc579753fc2079bbb83e9e9a8980f626
CloudChen <im_sure@163.com>
2016-12-25 20:11:07 +0800

    Add 343 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		int[] a = {1,2,3};
-		_384ShuffleAnArray test = new _384ShuffleAnArray(a);
-		int[] result = test.shuffle();
-		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
+		_343IntegerBreak test = new _343IntegerBreak();
+		int a = 54;
+		int result = test.integerBreak(a);
+		System.out.println(result);
 	}
 }
