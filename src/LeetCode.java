6ecd569c86c2d9ddff9221e88cd2fdb61081a090
CloudChen <im_sure@163.com>
2017-01-01 21:17:32 +0800

    Add 449 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,9 +3,16 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_012IntegerToRoman test = new _012IntegerToRoman();
-		int a = 3999;
-		String result = test.intToRoman(a);
-		System.out.println(result);
+		_449SerializeAndDeserializeBST test = new _449SerializeAndDeserializeBST();
+		_449SerializeAndDeserializeBST.TreeNode r = test.new TreeNode(1);
+		_449SerializeAndDeserializeBST.TreeNode rl = test.new TreeNode(2);
+		_449SerializeAndDeserializeBST.TreeNode rr = test.new TreeNode(3);
+		_449SerializeAndDeserializeBST.TreeNode rrl = test.new TreeNode(4);
+		r.left = rl;
+		r.right = rr;
+		rr.left = rrl;
+		String result = test.serialize(r);
+		_449SerializeAndDeserializeBST.TreeNode n = test.deserialize(result);
+		System.out.println(n.val + " " + n.left.val + " " + n.right.val + " " + n.right.left.val);
 	}
 }
---
+++ src/_449SerializeAndDeserializeBST.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _449SerializeAndDeserializeBST {
+	public String serialize(TreeNode root) {
+        if (root == null) return "n";
+		Queue<TreeNode> qRoot = new LinkedList<TreeNode>();
+        StringBuilder sb = new StringBuilder();
+		qRoot.offer(root);
+        sb.append(root.val);
+        while(!qRoot.isEmpty()) {
+    		Queue<TreeNode> qNode = new LinkedList<TreeNode>();
+            StringBuilder sbNode = new StringBuilder();
+            boolean isFirst = true;
+    		while(!qRoot.isEmpty()) {
+    			TreeNode node = qRoot.poll();
+    			if (!isFirst) sbNode.append(',');
+    			if (node.left != null) {
+    				qNode.offer(node.left);
+    				sbNode.append(node.left.val);
+    			} else sbNode.append('n');
+    			sbNode.append(',');
+    			if (node.right != null) {
+    				qNode.offer(node.right);
+    				sbNode.append(node.right.val);
+    			} else sbNode.append('n');
+    			isFirst = false;;
+    		}
+    		if (!qNode.isEmpty()) {
+    			sbNode.insert(0, ';');
+    			sb.append(sbNode);
+    		}
+    		qRoot = qNode;
+        }
+        return sb.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+    	TreeNode root;
+    	String[] sLevel = data.split(";");
+    	if (sLevel[0].equals("n")) return null;
+    	else root = new TreeNode(Integer.valueOf(sLevel[0]));
+		Queue<TreeNode> qRoot = new LinkedList<TreeNode>();
+    	qRoot.offer(root);
+    	for (int i = 1; i < sLevel.length; i++) {
+    		String[] sNode = sLevel[i].split(",");
+    		Queue<TreeNode> qNode = new LinkedList<TreeNode>();
+    		for (int j = 0; j < sNode.length; j++) {
+    			TreeNode node = qRoot.poll();
+    			String s = sNode[j];
+    			if (!s.equals("n")) {
+    				node.left = new TreeNode(Integer.valueOf(s));
+    				qNode.offer(node.left);
+    			}
+    			s = sNode[++j];
+    			if (!s.equals("n")) {
+    				node.right = new TreeNode(Integer.valueOf(s));
+    				qNode.offer(node.right);
+    			}
+    		}
+    		qRoot = qNode;
+    	}
+    	return root;
+    }
+    
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+}
