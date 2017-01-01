53656f50a6f47246012e0f7899b3c3e75b97d6a5
CloudChen <im_sure@163.com>
2017-01-01 14:37:14 +0800

    Revise 477

--- src/_477TotalHammingDistance.java
+++ src/_477TotalHammingDistance.java
@@ -5,8 +5,7 @@ public class _477TotalHammingDistance {
         int l = nums.length;
         for (int i = 0; i < 32; i++) {
         	int count = 0;
-        	int bit = 1 << i;
-        	for (int j = 0; j < l; j++) count += (bit & nums[j]) == 0 ? 0 : 1;
+        	for (int j = 0; j < l; j++) count += (nums[j] >> i) & 1;
         	total += count * (l - count);
         }
         return total;
