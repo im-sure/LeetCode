47a22b71989d61246cd7d87705f39a385fd05c5d
CloudChen <im_sure@163.com>
2017-01-02 19:24:20 +0800

    Add 436 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,16 +3,13 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_449SerializeAndDeserializeBST test = new _449SerializeAndDeserializeBST();
-		_449SerializeAndDeserializeBST.TreeNode r = test.new TreeNode(1);
-		_449SerializeAndDeserializeBST.TreeNode rl = test.new TreeNode(2);
-		_449SerializeAndDeserializeBST.TreeNode rr = test.new TreeNode(3);
-		_449SerializeAndDeserializeBST.TreeNode rrl = test.new TreeNode(4);
-		r.left = rl;
-		r.right = rr;
-		rr.left = rrl;
-		String result = test.serialize(r);
-		_449SerializeAndDeserializeBST.TreeNode n = test.deserialize(result);
-		System.out.println(n.val + " " + n.left.val + " " + n.right.val + " " + n.right.left.val);
+		_436FindRightInterval test = new _436FindRightInterval();
+		_436FindRightInterval.Interval a = test.new Interval(1, 2);
+		_436FindRightInterval.Interval b = test.new Interval(2, 3);
+		_436FindRightInterval.Interval c = test.new Interval(0, 1);
+		_436FindRightInterval.Interval d = test.new Interval(3, 4);
+		_436FindRightInterval.Interval[] r = {a, b, c, d};
+		int[] result = test.findRightInterval(r);
+		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
 	}
 }
