35da37e06f3f82d38ea2695d7a3f49ae14d19a09
CloudChen <im_sure@163.com>
2017-01-02 19:52:35 +0800

    Add 318 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,13 +3,9 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_436FindRightInterval test = new _436FindRightInterval();
-		_436FindRightInterval.Interval a = test.new Interval(1, 2);
-		_436FindRightInterval.Interval b = test.new Interval(2, 3);
-		_436FindRightInterval.Interval c = test.new Interval(0, 1);
-		_436FindRightInterval.Interval d = test.new Interval(3, 4);
-		_436FindRightInterval.Interval[] r = {a, b, c, d};
-		int[] result = test.findRightInterval(r);
-		for (int i = 0; i < result.length; i++) System.out.println(result[i]);
+		_318MaximumProductOfWordLengths test = new _318MaximumProductOfWordLengths();
+		String[] a = {"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+		int result = test.maxProduct(a);
+		System.out.println(result);
 	}
 }
