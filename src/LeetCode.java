c9249f92e9036fb3a7e9643343d978af5820ad94
CloudChen <im_sure@163.com>
2017-01-03 21:20:02 +0800

    Add 230 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,9 +3,17 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_452MinimumNumberOfArrowsToBurstBalloons test = new _452MinimumNumberOfArrowsToBurstBalloons();
-		int[][] a = {{1, 2}, {3, 4}, {5, 6}, {7,8}};
-		int result = test.findMinArrowShots(a);
+		_230KthSmallestElementInABST test = new _230KthSmallestElementInABST();
+		_230KthSmallestElementInABST.TreeNode t = test.new TreeNode(0);
+		_230KthSmallestElementInABST.TreeNode l = test.new TreeNode(-1);
+		_230KthSmallestElementInABST.TreeNode r = test.new TreeNode(2);
+		_230KthSmallestElementInABST.TreeNode rl = test.new TreeNode(1);
+		_230KthSmallestElementInABST.TreeNode rr = test.new TreeNode(3);
+		t.left = l;
+		t.right = r;
+		r.left = rl;
+		r.right = rr;
+		int result = test.kthSmallest(t, 4);
 		System.out.println(result);
 	}
 }
