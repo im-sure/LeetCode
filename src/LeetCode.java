8231bd0c919fa0d9d299413cae930534c99b6d83
CloudChen <im_sure@163.com>
2017-01-05 20:19:13 +0800

    Add 216 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,17 +3,8 @@ import java.util.List;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_230KthSmallestElementInABST test = new _230KthSmallestElementInABST();
-		_230KthSmallestElementInABST.TreeNode t = test.new TreeNode(0);
-		_230KthSmallestElementInABST.TreeNode l = test.new TreeNode(-1);
-		_230KthSmallestElementInABST.TreeNode r = test.new TreeNode(2);
-		_230KthSmallestElementInABST.TreeNode rl = test.new TreeNode(1);
-		_230KthSmallestElementInABST.TreeNode rr = test.new TreeNode(3);
-		t.left = l;
-		t.right = r;
-		r.left = rl;
-		r.right = rr;
-		int result = test.kthSmallest(t, 4);
-		System.out.println(result);
+		_216CombinationSumIII test = new _216CombinationSumIII();
+		List<List<Integer>> result = test.combinationSum3(2, 18);
+    	System.out.print(result);
 	}
 }
---
+++ src/_216CombinationSumIII.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _216CombinationSumIII {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+    	return findNum(k, 1, n);
+    }
+    
+    public List<List<Integer>> findNum(int k, int start, int n) {
+    	if (start > n) return null;
+    	List<List<Integer>> list = new ArrayList<List<Integer>>();
+    	if (k > 1) {
+    		for (int i = start; i < 10; i++) {
+				List<List<Integer>> nextList = findNum(k - 1, i + 1, n - i);
+				if (nextList == null) continue;
+				else {
+					for (List<Integer> l : nextList) l.add(i);
+					list.addAll(nextList);
+				}
+    		}
+		} else {
+			if (n >= start && n < 10) {
+				List<Integer> newList = new ArrayList<Integer>();
+				newList.add(n);
+				list.add(newList);
+			} else return null;
+		}
+    	return list;
+    }
+}
