db0850658fc1894d3a1b4b0bcd4845d81b135ef0
CloudChen <im_sure@163.com>
2017-01-08 19:33:48 +0800

    Add 328 solution

---
+++ src/_328OddEvenLinkedList.java
@@ -0,0 +1,33 @@
+
+public class _328OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+    	if (head == null) return null;
+    	else if (head.next == null) return head;
+    	ListNode firstOdd = head;
+    	ListNode firstEven = head.next;
+    	ListNode tmpOdd = head;
+    	ListNode tmpEven = head.next;
+    	ListNode thisNode = firstEven.next;
+    	boolean isOdd = true;
+    	while (thisNode != null) {
+    		if (isOdd) {
+    			tmpOdd.next = thisNode;
+    			tmpOdd = tmpOdd.next;
+    		} else {
+    			tmpEven.next = thisNode;
+    			tmpEven = tmpEven.next;
+    		}
+    		isOdd = !isOdd;
+    		thisNode = thisNode.next;
+    	}
+    	tmpOdd.next = firstEven;
+    	tmpEven.next = null;
+    	return firstOdd;
+    }
+    
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+}
