c2b24531d4d6f0a6c3967997c96940ee5d1bbeec
CloudChen <im_sure@163.com>
2017-02-18 20:44:31 +0800

    Add 173 solution

---
+++ src/_173BinarySearchTreeIterator.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class _173BinarySearchTreeIterator {
+	TreeNode root;
+	Stack<TreeNode> stack = new Stack<TreeNode>();
+
+    public _173BinarySearchTreeIterator(TreeNode root) {
+        this.root = root;
+        TreeNode tn = root;
+        while (tn != null) {
+        	stack.push(tn);
+        	tn = tn.left;
+        }
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    public int next() {
+    	TreeNode tn = stack.pop();
+    	int val = tn.val;
+        if (tn.right != null) {
+        	tn = tn.right;
+        	while (tn != null) {
+            	stack.push(tn);
+            	tn = tn.left;
+            }
+        }
+        return val;
+    }
+    
+    public class TreeNode {
+    	int val;
+    	TreeNode left;
+    	TreeNode right;
+    	TreeNode(int x) { val = x; }
+    }
+}
