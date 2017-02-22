c37718c0e7a0fdf8d4fcd6ca1033fec6b918bf60
CloudChen <im_sure@163.com>
2017-02-22 19:49:33 +0800

    Add 199 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5,9 +5,20 @@ import java.util.Set;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_390EliminationGame test = new _390EliminationGame();
-		int a = 9;
-		int result = test.lastRemaining(a);
+		_199BinaryTreeRightSideView test = new _199BinaryTreeRightSideView();
+		_199BinaryTreeRightSideView.TreeNode t = test.new TreeNode(1);
+		_199BinaryTreeRightSideView.TreeNode l = test.new TreeNode(2);
+		_199BinaryTreeRightSideView.TreeNode lr = test.new TreeNode(5);
+		_199BinaryTreeRightSideView.TreeNode lrl = test.new TreeNode(6);
+		_199BinaryTreeRightSideView.TreeNode r = test.new TreeNode(3);
+		_199BinaryTreeRightSideView.TreeNode rr = test.new TreeNode(4);
+		t.left = l;
+		l.right = lr;
+		lr.left = lrl;
+		t.right = r;
+		r.right = rr;
+//		int a = 9;
+		List<Integer> result = test.rightSideView(t);
     	System.out.println(result);
 	}
 }
