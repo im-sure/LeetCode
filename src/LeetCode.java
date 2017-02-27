f72302018da7d3341bd2f66fa3feecf219ccd527
CloudChen <im_sure@163.com>
2017-02-27 19:37:08 +0800

    Add 491 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5,20 +5,9 @@ import java.util.Set;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_199BinaryTreeRightSideView test = new _199BinaryTreeRightSideView();
-		_199BinaryTreeRightSideView.TreeNode t = test.new TreeNode(1);
-		_199BinaryTreeRightSideView.TreeNode l = test.new TreeNode(2);
-		_199BinaryTreeRightSideView.TreeNode lr = test.new TreeNode(5);
-		_199BinaryTreeRightSideView.TreeNode lrl = test.new TreeNode(6);
-		_199BinaryTreeRightSideView.TreeNode r = test.new TreeNode(3);
-		_199BinaryTreeRightSideView.TreeNode rr = test.new TreeNode(4);
-		t.left = l;
-		l.right = lr;
-		lr.left = lrl;
-		t.right = r;
-		r.right = rr;
-//		int a = 9;
-		List<Integer> result = test.rightSideView(t);
+		_491IncreasingSubsequences test = new _491IncreasingSubsequences();
+		int[] a = {4, 6, 7, 7};
+		List<List<Integer>> result = test.findSubsequences(a);
     	System.out.println(result);
 	}
 }
