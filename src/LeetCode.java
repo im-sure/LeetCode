c15c049c5efdb59c81a0cc4c79d85c6bbcab03fc
CloudChen <im_sure@163.com>
2017-03-15 23:09:04 +0800

    Add 395 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5,9 +5,10 @@ import java.util.Set;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_279PerfectSquares test = new _279PerfectSquares();
-		int[] a = {2,3,4,5,18,17,6};
-		int result = test.numSquares(287);
+		_395LongestSubstringWithAtLeastKRepeatingCharacters test = new _395LongestSubstringWithAtLeastKRepeatingCharacters();
+//		int[] a = {2,3,4,5,18,17,6};
+		String a = "abcdefghijklmnopqrstuvwxyz";
+		int result = test.longestSubstring(a, 2);
 		System.out.println(result);
 	}
 }
