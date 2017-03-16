cdd908142d4214700d9e1a4cd643f53c7b7124ba
CloudChen <im_sure@163.com>
2017-03-16 21:01:55 +0800

    Add 129 solution

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5,10 +5,17 @@ import java.util.Set;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_395LongestSubstringWithAtLeastKRepeatingCharacters test = new _395LongestSubstringWithAtLeastKRepeatingCharacters();
+		_129SumRootToLeafNumbers test = new _129SumRootToLeafNumbers();
+		_129SumRootToLeafNumbers.TreeNode t = test.new TreeNode(4);
+		_129SumRootToLeafNumbers.TreeNode l = test.new TreeNode(9);
+		_129SumRootToLeafNumbers.TreeNode r = test.new TreeNode(0);
+		_129SumRootToLeafNumbers.TreeNode lr = test.new TreeNode(1);
+		t.left = l;
+		t.right = r;
+		l.right = lr;
 //		int[] a = {2,3,4,5,18,17,6};
 		String a = "abcdefghijklmnopqrstuvwxyz";
-		int result = test.longestSubstring(a, 2);
+		int result = test.sumNumbers(t);
 		System.out.println(result);
 	}
 }
---
+++ src/_129SumRootToLeafNumbers.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class _129SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+    	if (root == null) return 0;
+        return sumNumbers(root, 0);
+    }
+    
+    private int sumNumbers(TreeNode root, int sum) {
+    	Stack<TreeNode> stack = new Stack<TreeNode>();
+    	stack.push(root);
+    	int tmpSum = sum * 10 + root.val;
+    	int s = 0;
+    	while (root.left != null) {
+    		stack.push(root.left);
+    		root = root.left;
+    		tmpSum = tmpSum * 10 + root.val;
+    	}
+    	if (root.right == null) s = tmpSum;
+    	while (!stack.isEmpty()) {
+    		if (stack.peek().right != null) {
+    			int a = sumNumbers(stack.peek().right, tmpSum);
+    			s += a;
+    		}
+			tmpSum /= 10;
+			stack.pop();
+    	}
+    	return s;
+    }
+    
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+}
