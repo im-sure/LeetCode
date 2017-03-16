976231d8c71e32a06167e9f92c7a1726201d2123
CloudChen <im_sure@163.com>
2017-03-16 22:02:37 +0800

    Add 331 solution

---
+++ src/_331VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,12 @@
+
+public class _331VerifyPreorderSerializationOfABinaryTree {
+    public boolean isValidSerialization(String preorder) {
+        String[] s = preorder.split(",");
+        int nulls = 1;
+        for (String str : s) {
+        	if (--nulls < 0) return false;
+        	if (!str.equals("#")) nulls += 2;
+        }
+        return nulls == 0;
+    }
+}
