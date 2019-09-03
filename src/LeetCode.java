65153c0c721ad542014aaf5a8d3e588226885bfa
Tingshuo Chen <tingshuo.chen@liulishuo.com>
2019-09-03 18:03:42 +0800

    Add _0133CloneGraph

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -5,9 +6,19 @@ import java.util.Set;
 
 public class LeetCode {
 	public static void main(String[] args) {
-		_0388LongestAbsoluteFilePath test = new _0388LongestAbsoluteFilePath();
-		String a = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
-		int result = test.lengthLongestPath(a);
-		System.out.println(result);
+		_0133CloneGraph test = new _0133CloneGraph();
+		_0133CloneGraph.Node n1 = test.new Node();
+		_0133CloneGraph.Node n2 = test.new Node();
+		_0133CloneGraph.Node n3 = test.new Node();
+		_0133CloneGraph.Node n4 = test.new Node();
+		n1.val = 1;
+		n1.neighbors = Arrays.asList(n2, n4);
+		n2.val = 2;
+		n2.neighbors = Arrays.asList(n1, n3);
+		n3.val = 3;
+		n3.neighbors = Arrays.asList(n2, n4);
+		n4.val = 4;
+		n4.neighbors = Arrays.asList(n1, n3);
+		System.out.println(test.cloneGraph(n1).toString());
 	}
 }
--- src/_0133CloneGraph.java
+++ src/_0133CloneGraph.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class _0133CloneGraph {
+    public Node cloneGraph(Node node) {
+    	Node newNode = new Node(node.val, new ArrayList<>());
+    	Map<Integer, Node> nodes = new HashMap<>();
+    	nodes.put(node.val, newNode);
+    	Map<Integer, Set<Integer>> visited = new HashMap<>();
+    	cloneGraph(node, newNode, nodes, visited);
+    	return newNode;
+    }
+    
+    public void cloneGraph(Node node, Node newNode, Map<Integer, Node> nodes, Map<Integer, Set<Integer>> visited) {
+		if (!visited.containsKey(node.val)) {
+			visited.put(node.val, new HashSet<>());
+		}
+    	for (Node neighbor : node.neighbors) {
+    		if (!visited.get(node.val).contains(neighbor.val)) {
+    			visited.get(node.val).add(neighbor.val);
+    			Node newNeighbor = null;
+    			if (nodes.containsKey(neighbor.val)) {
+    				newNeighbor = nodes.get(neighbor.val);
+    			} else {
+    				newNeighbor = new Node(neighbor.val, new ArrayList<>());
+    				nodes.put(neighbor.val, newNeighbor);
+    			}
+    			newNode.neighbors.add(newNeighbor);
+    			cloneGraph(neighbor, newNeighbor, nodes, visited);
+    		}
+    	}
+    }
+    
+    public class Node {
+        public int val;
+        public List<Node> neighbors;
 
+        public Node() {}
+
+        public Node(int _val, List<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
 }
