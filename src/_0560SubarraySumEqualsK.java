644544d2a81d72d024296777773d924d6faf621f
Tingshuo Chen <tingshuo.chen@liulishuo.com>
2020-11-13 16:25:58 +0800

    Minor

--- src/_0560SubarraySumEqualsK.java
+++ src/_0560SubarraySumEqualsK.java
@@ -8,33 +8,21 @@ public class _0560SubarraySumEqualsK {
     int[] sums = new int[nums.length];
     Map<Integer, List<Integer>> indexes = new HashMap<>();
     int sum = 0;
+    int res = 0;
     for (int i = 0; i < nums.length; i++) {
       sum += nums[i];
+      if (sum == k) {
+        res++;
+      }
+      int diff = sum - k;
+      if (indexes.containsKey(diff)) {
+        res += indexes.get(diff).size();
+      }
       sums[i] = sum;
       if (!indexes.containsKey(sum)) {
         indexes.put(sum, new ArrayList<>());
       }
       indexes.get(sum).add(i);
-    }
-    System.out.println(indexes);
-    int res = 0;
-    for (int i = 0; i < sums.length; i++) {
-      int val = sums[i];
-      if (val == k) {
-        res++;
-      }
-      int diff = val - k;
-      System.out.println("*******diff: " + diff);
-      if (indexes.containsKey(diff)) {
-        for (Integer index : indexes.get(diff)) {
-          System.out.println("*******index: " + index);
-          if (index < i) {
-            res++;
-          } else {
-            break;
-          }
-        }
-      }
     }
     return res;
   }
