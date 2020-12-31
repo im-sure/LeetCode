feecbcf28898038bf186b1934fa282bf74be6109
Tingshuo Chen <tingshuo.chen@liulishuo.com>
2020-12-31 16:52:38 +0800

    Add _0287FindTheDuplicateNumber

--- src/_0287FindTheDuplicateNumber.java
+++ src/_0287FindTheDuplicateNumber.java
@@ -1,5 +1,18 @@
 public class _0287FindTheDuplicateNumber {
   public int findDuplicate(int[] nums) {
-
+    int slow = 0;
+    int fast = 0;
+    while (true) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+      if (slow == fast) {
+        fast = 0;
+        while (nums[slow] != nums[fast]) {
+          slow = nums[slow];
+          fast = nums[fast];
+        }
+        return nums[slow];
+      }
+    }
   }
 }
