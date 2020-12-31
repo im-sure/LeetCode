f98fdb51e56c72feee962e1860485a108318c689
Tingshuo Chen <tingshuo.chen@liulishuo.com>
2020-12-31 16:04:15 +0800

    Add _0312BurstBalloons

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class LeetCode {
 	public static void main(String[] args) {
-    _0128LongestConsecutiveSequence test = new _0128LongestConsecutiveSequence();
-    System.out.println(test.longestConsecutive(new int[]{100,4,200,1,3,2}));
+    _0312BurstBalloons test = new _0312BurstBalloons();
+    System.out.println(test.maxCoins(new int[]{3,1,5,8}));
 	}
 }
--- src/_0312BurstBalloons.java
+++ src/_0312BurstBalloons.java
@@ -1,3 +1,31 @@
 public class _0312BurstBalloons {
-  
+  public int maxCoins(int[] nums) {
+    int length = nums.length;
+    int[] newNums = new int[length + 2];
+    for (int i = 0; i < length + 2; i++) {
+      if (i == 0 || i == length + 1) {
+        newNums[i] = 1;
+      } else {
+        newNums[i] = nums[i - 1];
+      }
+    }
+    int dp[][] = new int[length + 1][length + 1];
+    return dp(newNums, dp, 1, length);
+  }
+
+  private int dp(int[] nums, int[][] dp, int start, int end) {
+    if (end < start) {
+      return 0;
+    }
+    if (dp[start][end] == 0) {
+      if (start == end) {
+        dp[start][end] = nums[start - 1] * nums[start] * nums[start + 1];
+      } else {
+        for (int i = start; i <= end; i++) {
+          dp[start][end] = Math.max(dp[start][end], dp(nums, dp, start, i - 1) + nums[start - 1] * nums[i] * nums[end + 1] + dp(nums, dp, i + 1, end));
+        }
+      }
+    }
+    return dp[start][end];
+  }
 }
